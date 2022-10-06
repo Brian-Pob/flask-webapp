18e3be371c39c2f3bb9be20fcd564a4f041583d4
Brian Poblete <brianpoblete77777@gmail.com>
2022-10-06 02:34:09 +0000

    implemented auth0

--- cop4521.py
+++ cop4521.py
@@ -1,14 +1,67 @@
-from flask import Flask
+import time
+import json
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
+
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, session, url_for
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
 
 app = Flask(__name__)
+app.secret_key = env.get("APP_SECRET_KEY")
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
+
 
 @app.route("/")
+def index():
+    #to_return = "<img src='/static/images/stars.jpg' alt='no image found'>"
+    #image_url = url_for('memes', filename='spongebob-leak.gif')
+    image_url = '/memes/spongebob-leak.gif'
+    to_return = "<img src='%s' alt='no image found!!'>" % (image_url)
+    return '<p>This is a Python Flask app running with Gunicorn and Nginx! 🐍+🧪+🦄+🚙 = ⚡️💪🔥</p>' + to_return + image_url
 
-def test_func():
-	return "<p>This is a new function!</p>"
+@app.route("/login")
+def login():
+    print(url_for("callback", _external=True))
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
 
-def hello_world():
-    return "<p>This is a Python Flask app running with Gunicorn and Nginx! 🐍+🧪+🦄+🚙 = ⚡️💪🔥</p>"
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://" + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("index", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
