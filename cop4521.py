2b6037057f382e53a0514b052e00b30b1104d5e6
Brian Poblete <brianpoblete77777@gmail.com>
2022-10-07 16:47:18 +0000

    added base.html

--- cop4521.py
+++ cop4521.py
@@ -26,14 +26,20 @@ oauth.register(
     server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
 )
 
-
 @app.route("/")
 def index():
     #to_return = "<img src='/static/images/stars.jpg' alt='no image found'>"
     #image_url = url_for('memes', filename='spongebob-leak.gif')
     image_url = '/memes/spongebob-leak.gif'
     to_return = "<img src='%s' alt='no image found!!'>" % (image_url)
+    session_user = dict(session).get('user', None)
+    if (session_user):
+        to_return = to_return + "<p>User {} is logged in</p>".format(session_user["userinfo"]["name"])
     return '<p>This is a Python Flask app running with Gunicorn and Nginx! 🐍+🧪+🦄+🚙 = ⚡️💪🔥</p>' + to_return + image_url
+
+@app.route("/home")
+def home():
+    return render_template("home.html", session=dict(session).get('user', None)) 
 
 @app.route("/login")
 def login():
