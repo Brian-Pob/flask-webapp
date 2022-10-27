564b3458ad40559a9fd8604eba311474df4319c0
Brian Poblete <brianmp.dev@gmail.com>
2022-10-26 23:02:02 -0400

    buttons changed color if not logged in

---
+++ migrations/versions/99e1b8e7a114_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 99e1b8e7a114
+Revises: ed5a0a76b31a, a828c7a170c7
+Create Date: 2022-10-26 22:59:37.916138
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '99e1b8e7a114'
+down_revision = ('ed5a0a76b31a', 'a828c7a170c7')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
