9a21e7ac7b715c34d7d5c56b3bfdc86de817cb34
Brian Poblete <brianmp.dev@gmail.com>
2022-11-07 00:07:10 -0500

    reimplemented caching. now with filesystem. left unused async code

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     DEBUG = True
-    CACHE_TYPE = "SimpleCache"
+    CACHE_TYPE = "FileSystemCache"
+    CACHE_DIR = "flaskcache/"
+    CACHE_OPTIONS = {'mode':777}
     CACHE_DEFAULT_TIMEOUT = 300
     SQLALCHEMY_TRACK_MODIFICATIONS = False
